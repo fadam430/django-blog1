846cc88a6616d10c151513a399f716f11216e515
fadam430 <f.adam430@gmail.com>
2025-11-20 09:57:13 +0000

    add post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render # type: ignore
+from django.shortcuts import render, get_object_or_404 # type: ignore
 from django.views import generic # type: ignore
 from .models import Post
 
@@ -7,3 +7,21 @@ class PostList(generic.ListView):
     queryset = Post.objects.all()
     template_name = 'blog/index.html'
     paginate_by = 6
+    
+def post_detail(request,  slug):
+    """
+    Renders the detail view of a blog post identified by its slug.
+    
+    **Content**
+    
+    ``post_detail``
+        an instance of model : `blog.pot`
+        
+    **Template**
+    
+    :template:`blog/post_detail.html`
+    """
+    
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(request, 'blog/post_detail.html', {'post': post}) 
