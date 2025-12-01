6478cd4e31f7c98d77c0df96c4b15dc8e8243624
fadam430 <f.adam430@gmail.com>
2025-12-01 17:06:54 +0000

    add some docstring

--- about/models.py
+++ about/models.py
@@ -1,15 +1,31 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class About(models.Model):
+    """
+    Renders the most recent information on the website author and allows user collaboration requests.
+    Displays an individual instance of :model:`about.About`
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`
+    ``collaborate_form``
+        An instance of :form:`about.CollaborateForm`
+    **Template**
+    :template:`about/about.html`
+    """
     title = models.CharField(max_length=200)
     content = models.TextField()
+    profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
     
 class CollaborateRequest(models.Model):
+    """
+    Store collaboration request details.
+    """
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,11 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
 class Post(models.Model):
+    """
+    Store a single blog post entry related to :model:`auth.User`.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name ='blog_posts')
@@ -21,6 +25,9 @@ class Post(models.Model):
         return f"{self.title} | written by {self.author}"
     
 class Comment(models.Model):
+    """
+    Store a single comment related to a :model:`auth.User` and  :model:`blog.Post`.
+    """
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
