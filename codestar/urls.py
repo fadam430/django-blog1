250821c3644a4223b74518a125396cfa369f3db3
fadam430 <f.adam430@gmail.com>
2025-12-09 11:35:18 +0000

    add custome image for 1 post

--- codestar/urls.py
+++ codestar/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('about/', include('about.urls'), name='about-urls'),
-    path('', include('blog.urls'), name='blog-urls'),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('blog.urls'), name='blog-urls'),
 ]
