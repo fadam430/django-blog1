736d26f1c43b037dfaa5acc7607eb30d3ca2fb0f
fadam430 <f.adam430@gmail.com>
2025-12-09 10:05:20 +0000

    some small changes

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+from django.db import models # type: ignore
+from django.contrib.auth.models import User # type: ignore
+from cloudinary.models import CloudinaryField # type: ignore
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
